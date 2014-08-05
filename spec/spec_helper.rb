b1ec1ae216081797264a497ef73d73608bdbf025
Bryce Kalow <bryce.kalow@digi.com>
2014-08-05 18:25:42 -0500

    Added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rack/test/'
+
+require File.expand_path '../../app.rb', __FILE__
+
+ENV['RACK_ENV'] == 'test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() DigiFoosball::Base end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
