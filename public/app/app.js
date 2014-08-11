0a33c0161387e68a194c626c90f5970b60bcd106
Bryce Kalow <bryce.kalow@digi.com>
2014-08-11 14:53:50 -0500

    Spacing

--- public/app/app.js
+++ public/app/app.js
@@ -11,7 +11,6 @@ var app = angular.module('DigiFoosball', [
   'digiFoosballServices'
 ]);
 
-
 app.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
