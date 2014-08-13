85f1f4b25797da469ed52a2d857a27d3bc5a67b6
Bryce Kalow <bryce.kalow@digi.com>
2014-08-13 14:13:45 -0500

    Made charts not be incorrect at 0 points scored

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -150,14 +150,14 @@ digiFoosballControllers.controller('GameCtrl', function($scope, $routeParams, Ga
         var away_score = 0;
         var data_points = [{x:"0",y:[0,0]}];
 
-        for(var i = 1; i <= score_history.length; i++) {
-            if (score_history[i-1] == 'home') {
+        for(var i = 0; i <= score_history.length; i++) {
+            if (score_history[i] == 'home') {
                 home_score += 1;
-            } else {
+            } else if(score_history[i] == 'away') {
                 away_score += 1;
             }
 
-            data_points.push({x: i.toString(), y: [home_score,away_score]})
+            data_points.push({x: (i+1).toString(), y: [home_score,away_score]})
         }
 
         return data_points;
