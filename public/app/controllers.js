0e35034482dfafdeb6b9ad3471e3730aa0e119c9
Bryce <br.kalow@gmail.com>
2014-08-16 20:39:10 -0500

    Added a score history chart service

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -168,71 +168,28 @@ digiFoosballControllers.controller('GameListCtrl', function($scope, Game) {
     $scope.games = Game.allGames();
 });
 
-digiFoosballControllers.controller('GameCtrl', function($scope, $routeParams, Game) {
+digiFoosballControllers.controller('GameCtrl', function($scope, $routeParams, Game, scoreChart) {
     $scope.$parent.title = "Game";
     $scope.game = Game.resource.get({gameId:$routeParams.gameId});
+    $scope.chart = scoreChart.getChart();
+    $scope.rebuildChart = function() {
+        scoreChart.rebuildChartConfig($scope.game.score_history, $scope.game.player_home.name, $scope.game.player_away.name);
+    };
 
     /**
-    * EventStream related declarations
+    * Stream
     */
     $scope.$on('game-push-received', function(event, args) {
         var receivedGame = args.receivedGame;
         if(receivedGame.id == $routeParams.gameId) {
             $scope.game = receivedGame;
-            buildChartConfig();
+            $scope.rebuildChart();
+            $scope.chart = scoreChart.getChart();
         }
     });
 
-    /**
-    * Construct score history graph
-    */
-    var buildDataPoints = function(points) {
-        var score_history = points.split(",");
-        var home_score = 0;
-        var away_score = 0;
-        var data_points = [{x:"0",y:[0,0]}];
-
-        for(var i = 0; i <= score_history.length; i++) {
-            if (score_history[i] == 'home') {
-                home_score += 1;
-            } else if(score_history[i] == 'away') {
-                away_score += 1;
-            }
-
-            data_points.push({x: (i+1).toString(), y: [home_score,away_score]});
-        }
-
-        return data_points;
-    };
-
-    var buildChartConfig = function() {
-        $scope.chartType = 'line';
-        $scope.config = {
-            labels: false,
-            title : "",
-            legend : {
-                display: false,
-                position:'right'
-            }
-        };
-
-        $scope.data = {
-            series: [$scope.game.player_home.name, $scope.game.player_away.name],
-            data : buildDataPoints($scope.game.score_history)
-        };
-    };
-
-    $scope.chartType = 'line';
-    $scope.config = {
-        labels: false,
-        title : "",
-        legend : {
-            display: false,
-            position:'right'
-        }
-    };
-
     $scope.game.$promise.then(function() {
-        buildChartConfig();
+        $scope.rebuildChart();
+        $scope.chart = scoreChart.getChart();
     });
 });
