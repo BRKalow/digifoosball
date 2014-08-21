62ee856fc0dee8712fa49dc461f89eb52e7804ce
Bryce Kalow <bryce.kalow@digi.com>
2014-08-21 08:47:28 -0500

    Added a live game clock, resolve #1

--- public/app/directives.js
+++ public/app/directives.js
@@ -51,3 +51,27 @@ app.directive('userStatBar', function() {
     templateUrl: 'app/partials/user-stat-bars.tpl.html'
   };
 });
+
+app.directive('gameClock', function($timeout) {
+  return {
+    restrict: 'AE',
+    template: '<div class="game-clock">{{ difference() }}</div>',
+    link: function(scope, elem, attrs) {
+      scope.startTime = new Date(attrs.start).valueOf();
+      scope.currentTime = new Date().valueOf();
+      scope.difference = function() { 
+        return Math.round((scope.currentTime - scope.startTime) / 10) / 100; 
+      };
+      scope.updateTime = function() {
+        $timeout(function() {
+          scope.trigger = new Date();
+          scope.updateTime();
+        }, 100);
+      };
+      scope.updateTime();
+      scope.$watch(function() { return (new Date()).valueOf(); }, function(newVal) {
+        scope.currentTime = newVal;
+      });
+    }
+  };
+});
