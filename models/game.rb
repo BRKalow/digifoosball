1aa802c882c0a95987d0b9acd3f68282c758884e
Bryce Kalow <bryce.kalow@digi.com>
2014-09-10 10:43:08 -0500

    Updated another part of the ranking algorithm

--- models/game.rb
+++ models/game.rb
@@ -70,7 +70,7 @@ class Game < ActiveRecord::Base
   def determine_rating_change
     scores = self.score_per_team
     goal_weight = (scores['home'] - scores['away']).abs / 10.0
-    rating_diff = self.loser.rating - self.winner.rating 
+    rating_diff = (self.loser.rating - self.winner.rating).abs
 
     rating = Integer 10*(1 + (goal_weight - (1 / (1.0 + 10**((rating_diff) / 400.0)))))    
     rating = 0 if rating < 0
